a5f2ce884eb8764a25227251d1c5452b17c236db
Felipe Lema <felipelema@mortemale.org>
2020-09-28 17:09:49 -0300

    Complete internal README

--- julia-formatter.el
+++ julia-formatter.el
@@ -25,19 +25,23 @@
 ;;; Commentary:
 
 ;; Leverage JuliaFormatter.jl to indent julia code in Emacs.
+;;
 ;; Provide formatting tools for live coding.  These tools are packed into a service
 ;; that can be called using JSON-RPC on stdin / stdout.  Exposing JuliaFormatter.jl
-;; as a service is because the compile time required to get the result of that
+;; as a service because the compile time required to get the result of that
 ;; first format_text() call is considerable and it hinders the coding process.
-;; TODO felipel: extend this commentary for MELPA, probably mention `indent-region-function'
 ;;
 ;; The code that's being formatted must be self-contained (parseable, all if's
 ;; and while's with a corresponding end).  This is a requirement from
 ;; JuliaFormatter.jl since it needs to get the AST from the code.
 ;;
-;; Usage (put this in your init.el):
+;; The simplest way to use JuliaFormatter.jl is by activating `aggressive-indent-mode'
+;; and setting the proper functions
+;;
+;; Like so:
 ;; ;; load this file after downloading this package (or installing with straight.el)
 ;; (require 'julia-formatter)
+;; ;; load aggressive indent and setup appropiate variables
 ;; (julia-formatter-setup-hooks)
 ;;
 ;; See https://github.com/domluna/JuliaFormatter.jl

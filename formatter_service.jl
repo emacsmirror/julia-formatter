1943a1f031151ca1e3852fe8c9559dd296d912ba
Felipe Lema <felipelema@mortemale.org>
2023-03-11 14:24:09 -0300

    merge empty Dict, not `nothing` fix #10

--- formatter_service.jl
+++ formatter_service.jl
@@ -1,15 +1,15 @@
 # Copyright © 2020 Felipe Lema
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -53,7 +53,7 @@ function format_data(rpc_message)
 
     out_text = text_to_format
 
-    toml_config = rpc_message["params"]["toml_config"]
+    toml_config = something(rpc_message["params"]["toml_config"], Dict())
     preferred_config=Dict(
         # options that will not alter the number of lines in text
         # why? because altering the lines of text will confuse the users
